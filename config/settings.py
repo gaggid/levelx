59b84ea287b941df275381c47e964f1ce716e281
gaggid <gagan.mehrok@gmail.com>
2025-11-25 00:41:08 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     DEBUG: bool = True
 
-    GEMINI_API_KEY: Optional[str] = "AIzaSyDs7_5vlXkUrj5nI-SDTExOZHTVIDVsv_U"
+    GEMINI_API_KEY: Optional[str] = None
 
     # Database
     SUPABASE_URL: str
