469290558de0515c3d3cf550c8dfc582b43037ba
Gagan <gagan.mehrok@gmail.com>
2025-11-26 19:53:03 +0530

    Clean Up

--- frontend/components/landing/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-slate-800 py-12 px-4">
-      <div className="max-w-6xl mx-auto text-center">
-        <p className="text-slate-500">© 2025 LevelX. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
